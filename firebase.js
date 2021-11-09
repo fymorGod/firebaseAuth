b82dab2932c7210b9c8b2cea030877a0ffbf105a
fymorGod <mufinagamer@gmail.com>
2021-11-09 20:46:53 -0300

    second commit

---
+++ firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from "firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_1wRPPqvum4QBZd5CeH_af3SkpNeSy6g",
+  authDomain: "hroapp-d72fc.firebaseapp.com",
+  projectId: "hroapp-d72fc",
+  storageBucket: "hroapp-d72fc.appspot.com",
+  messagingSenderId: "64303262637",
+  appId: "1:64303262637:web:aa72d16b3cc68e89ed0f24"
+};
+
+// Initialize Firebase
+let app;
+if (firebase.apps.length === 0){
+    app = firebase.initializeApp(firebaseConfig);
+}else{
+    app = firebase.app()
+}
+
+const auth = firebase.auth()
+
+export { auth };
